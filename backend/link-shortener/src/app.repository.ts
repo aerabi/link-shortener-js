003ac523c1901ef859128e96596a782ec7c5ec7c
Collabnix <34368930+collabnix@users.noreply.github.com>
2022-06-29 17:43:34 +0530

    Update app.repository.ts

--- backend/link-shortener/src/app.repository.ts
+++ backend/link-shortener/src/app.repository.ts
@@ -1,8 +1,6 @@
-import { Observable } from 'rxjs';
-
 export interface AppRepository {
-  put(hash: string, url: string): Observable<string>;
-  get(hash: string): Observable<string>;
+  put(hash: string, url: string): Promise<void>;
+  get(hash: string): Promise<string>;
 }
 
 export const AppRepositoryTag = 'AppRepository';
